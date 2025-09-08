a680b16bee593915dcf3f562c15e28153ef52536
Rafaela <rafaelabezerra133@gmail.com>
2025-09-08 17:00:11 -0300

    chore(lambda): ajustes finais no processamento de arquivos

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -10,6 +10,7 @@ from gemini_processor import analisar_com_gemini
 
 s3 = boto3.client('s3')
 BUCKET_NAME = os.environ.get('BUCKET_NAME') 
+
 def lambda_handler(event, context):
     try:
         headers = event.get('params', {}).get('header', {})
@@ -19,7 +20,6 @@ def lambda_handler(event, context):
             raise ValueError("Header 'Content-Type' não encontrado na requisição.")
 
         body_decodificado = base64.b64decode(event['body-json'])
-
         msg = email.message_from_bytes(b'Content-Type: ' + content_type.encode() + b'\r\n' + body_decodificado)
 
         respostas = []
@@ -44,7 +44,18 @@ def lambda_handler(event, context):
                 if not nota_fiscal:
                     raise ValueError("Gemini não retornou dados válidos da nota.")
 
-                nota_fiscal['s3_location'] = f's3://{BUCKET_NAME}/{upload_key}'
+                forma_pgto = (nota_fiscal.get("forma_pgto") or "").lower()
+                pasta_final = "dinheiro" if forma_pgto in ["dinheiro", "pix"] else "outros"
+                novo_caminho = f"{pasta_final}/{filename}"
+
+                s3.copy_object(
+                    Bucket=BUCKET_NAME,
+                    CopySource={"Bucket": BUCKET_NAME, "Key": upload_key},
+                    Key=novo_caminho
+                )
+                s3.delete_object(Bucket=BUCKET_NAME, Key=upload_key)
+
+                nota_fiscal['s3_location'] = f's3://{BUCKET_NAME}/{novo_caminho}'
 
                 respostas.append({
                     "arquivo": part.get_filename(),
